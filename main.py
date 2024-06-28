c774f499b1ed23666f8634cc81aee4a1c5f863cb
Eber Lugarezi <eberqueiroz333@gmail.com>
2024-06-28 10:32:34 -0300

    media aluno 2

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-claudioNotas = [10, 7, 5, 6 ]
-joaoNotas = [3, 5, 6 ,5]
-thiagoNotas = [10, 7, 7, 9]
-samuelNotas = [10, 9, 9, 8]
-
-claudioMedia = sum(claudioNotas ) / 4
-joaoMedia =   sum (joaoNotas) / 4
-thiagoMedia = sum(thiagoNotas ) / 4
-samuelMedia = sum (samuelNotas) / 4
-
-if claudioMedia >= 5:
-    claudioAprovaçao = 'Aprovado'
-else:
-    claudioAprovaçao = 'Reprovado'
-
-if joaoMedia >= 5:
-    joaoAprovaçao = 'Aprovado'
-else:
-    joaoAprovaçao = 'Reprovado'
-
-if thiagoMedia >= 5:
-    thiagoAprovaçao = 'Aprovado'
-else:
-  thiagoAprovaçao = 'Reprovado'
-
-if samuelMedia>= 5:
-    samuelAprovaçao = 'Aprovado'
-else:
-    samuelAprovaçao = 'Reprovado'
+try:
+    nomeAluno = (input('Digite o nome do aluno: ')).title()
+    primeiraNota = int(input('Primeira Nota do aluno: '))
+    segundaNota = int(input('Segunda Nota do aluno: '))
+    terceiraNota = int(input('terceira Nota do aluno: '))
+    quartaNota = int(input('quarta Nota do aluno: '))
 
 
-print('===\tNota média doa alunos\t===')
-print(f'Claudia: {claudioMedia}\t {claudioAprovaçao}\nJoão: {joaoMedia}\t {joaoAprovaçao}\nThiago: {thiagoMedia}\t {thiagoAprovaçao}\nSamuel: {samuelMedia}\t {samuelAprovaçao} ')
+    mediaTotal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
 
 
+
+    if mediaTotal >= 5:
+        alunoAprovacao = 'Aprovado'
+    else:
+        alunoAprovacao = 'Reprovado'
+
+    # Mostra a nota do aluno
+    print(f'\tNota média do aluno {nomeAluno}\t')
+    print(f'\nMédia: {mediaTotal}\n1° Bimestre: {primeiraNota}\n2° Bimestre: {segundaNota}\n3° Bimestre: {terceiraNota}\n4° Bimestre: {quartaNota}')
+    print(f'\nSITUAÇÃO: {alunoAprovacao}')
+    
+except:
+    print('Ocorreu um erro...')
